1b312e6274f57ee9f7559cfdd15a3b10fe0684e0
Nick <nickbarkerpdx@gmail.com>
2022-05-15 11:51:40 -0700

    at it again

---
+++ Python/Marky Marker.py
@@ -0,0 +1,18 @@
+#This is a comment
+
+star = '*'
+
+print(star * 5 + "Welcome to the Funky Buncher"+ star * 5)
+#print("WELCOME TO THE FUNKY BUNCHER!")
+
+username = input("enter your name here :")
+
+if username == "Marky Mark":
+    print("Ah yes, Marcus, I shall name you:" )
+    username = "Marky Mark and the Funky Bunch"
+    print(username)
+else:
+    pass
+
+
+print("Username is: " + username)
